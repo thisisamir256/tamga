991bd209077089f8eb769e794b926bb9bc238efc
amir <amircb1367@gmail.com>
2025-09-02 19:31:55 +0330

    update

--- functions.php
+++ functions.php
@@ -2,8 +2,20 @@
 add_action( 'wp_enqueue_scripts', 'amir_enqueue_styles' );
 
 function amir_enqueue_styles() {
-	wp_enqueue_style( 
-		'tamga', 
-		get_stylesheet_uri()
-	);
+    // استایل قالب والد
+    wp_enqueue_style(
+        'twentytwentyfive-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    // استایل قالب فرزند
+    wp_enqueue_style(
+        'tamga-style',
+        get_stylesheet_uri(),
+        array('twentytwentyfive-style') // وابستگی به استایل والد
+    );
 }
+
+
+// Remove the category count for WooCommerce categories
+add_filter( 'woocommerce_subcategory_count_html', '__return_null' );
